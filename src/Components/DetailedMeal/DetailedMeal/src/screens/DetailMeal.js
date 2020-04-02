4e1bd243802ddde8f79d49685667f237855fd2a7
mooreit <>
2020-04-02 16:41:17 -0500

    changed the like and dislike buttion of detail screen

--- src/Components/DetailedMeal/DetailedMeal/src/screens/DetailMeal.js
+++ src/Components/DetailedMeal/DetailedMeal/src/screens/DetailMeal.js
@@ -216,255 +216,302 @@ function DetailMeal(props) {
   // getMealInfo();
 
   return (
-
+          
     <ScrollView style={styles.container}>
-
-      <View>
+      <View style={styles.group}>
         <Image
           source={imgSrc}
-          resizeMode="cover"
+          resizeMode="contain"
           style={styles.image}
         ></Image>
       </View>
 
-   
-
       <View>
-
-        <View style={{ flexDirection: "row", marginVertical: 20 }}>
-          <Text style={{ fontWeight: "bold", fontSize: 35, marginLeft: wp("10%"), flex: 5 }}>
+        <View style={{flexDirection:"row"}}>
+          <Text style={{fontWeight:"bold", fontSize:40, marginLeft:wp("10%")}}>
             {mealInfo.name}
           </Text>
-
-          <Text style={{ fontWeight: "bold", fontSize: 30, color: "rgba(106,164,27,1)", paddingLeft: wp("5%") }}>
+          <Text style={{fontWeight:"bold", fontSize:40, color:"rgba(106,164,27,1)", paddingLeft:wp("5%")}}>
             {mealInfo.price}
           </Text>
         </View>
 
-         
         <View style={{ flexDirection: "row", flex: 3, }}>
           <View style={{ marginHorizontal: 60 }}>
             <TouchableOpacity onPress={addToFavorite2}>
-              {/* <AntDesign name='like1' size={30} color='#ed2728'/> */}
-              <Ionicons name='ios-heart' size={30} color='#ed2728' />
+              <AntDesign name='like1' size={30} color='#ed2728'/>
+              {/* <Ionicons name='ios-heart' size={30} color='#ed2728' /> */}
             </TouchableOpacity>
           </View>
 
           <TouchableOpacity onPress={removeToFavorite2}>
-            {/* <SimpleLineIcons name='dislike' size={28} color='#77787d'/> */}
-            <Ionicons name='ios-heart-dislike' size={28} color='#77787d' />
+          <AntDesign name='dislike1' size={30} color='#77787d'/>
+            {/* <Ionicons name='ios-heart-dislike' size={28} color='#77787d' /> */}
           </TouchableOpacity>
         </View>
         
-        <View style={{ flexDirection: "row", marginVertical: 20 }}>
-          <Text style={{ fontWeight: "bold", fontSize: 20, alignSelf: "center", paddingLeft: wp("5%") }}>
+        <View style={{flexDirection:"row", marginVertical:20}}>
+          <Text style={{fontWeight:"bold", fontSize:20, alignSelf:"center", paddingLeft:wp("5%")}}>
             Serving Size:
           </Text>
-          <TextInput style={{ borderWidth: 1, marginTop: 5, width: 34, height: 30, marginLeft: wp("1%"), alignSelf: "center" }} keyboardType="numeric" placeholder="qty"
-            onChangeText={text => setQty(text)}
-          />
+          <TextInput style={{borderWidth:1 ,marginTop:5, width:34, height:30, marginLeft:wp("1%"), alignSelf:"center"}} keyboardType="numeric" placeholder="qty"
+                      onChangeText={text => setQty(text)}
+                      />
+          
+          {/* <Text style={{fontWeight:"bold", fontSize:20, alignSelf:"center", paddingLeft:wp("5%")}}>
+            Pick-up Location:
+          </Text>
+          <TextInput style={{borderWidth:1 ,marginTop:5, width:wp("30%"), height:hp("4%"), marginLeft:wp("1%"), alignSelf:"center"}}/> */}
+          
+          {/* <TouchableOpacity style={{backgroundColor:"rgba(106,164,27,1)", width:wp("1%"), alignItems:"center", marginLeft:wp("5%") , padding:10, borderRadius:10}}
+                onPress={() => {navigation.navigate("Shopping Cart")}}
+          >
+              <Text style={{color:"white"}}>
+                Go to Shopping Cart
+              </Text>
+          </TouchableOpacity> */}
 
-          <TouchableOpacity style={{ backgroundColor: "#a1c559", width: wp("20%"), alignItems: "center", padding: 10, borderRadius: 10, position: "absolute", alignSelf: "center", right: wp("1%") }}
-            onPress={addToCart}>
-            <Text style={{ color: "white" }}>
-              Add to Shopping Cart
+          <TouchableOpacity style={{backgroundColor:"rgba(106,164,27,1)", width:wp("20%"), alignItems:"center", padding:10, borderRadius:10, position:"absolute", alignSelf:"center", right:wp("1%")}}
+                onPress={addToCart}
+          >
+              <Text style={{color:"white"}}>
+                Add to Shopping Cart
               </Text>
           </TouchableOpacity>
         </View>
 
-
-        <View style={{ flexDirection: "row", marginBottom: hp("2%") }}>
-          <Text style={{ fontWeight: "bold", fontSize: 20, paddingLeft: wp("5%") }}>
+        <View style={{flexDirection:"row", marginBottom:hp("2%")}}>
+          <Text style={{fontWeight:"bold", fontSize:20, paddingLeft:wp("5%")}}>
             Pick-up Location:
           </Text>
-          <TextInput style={{ borderWidth: 1, marginTop: 5, width: wp("30%"), height: hp("3%"), marginLeft: wp("1%") }} />
+          <TextInput style={{borderWidth:1 ,marginTop:5, width:wp("30%"), height:hp("3%"), marginLeft:wp("1%")}}/> 
 
-          <TouchableOpacity style={{ backgroundColor: "#a1c559", width: wp("20%"), alignItems: "center", marginLeft: wp("5%"), padding: 10, borderRadius: 10, position: "absolute", right: wp("1%") }}
-            onPress={() => { navigation.navigate("Shopping Cart") }}>
-            <Text style={{ color: "white" }}>
-              Go to Shopping Cart <Feather name="shopping-cart" color="white" />
-            </Text>
+           <TouchableOpacity style={{backgroundColor:"rgba(106,164,27,1)", width:wp("20%"), alignItems:"center", marginLeft:wp("5%") , padding:10, borderRadius:10, position:"absolute",right:wp("1%")}}
+                onPress={() => {navigation.navigate("Shopping Cart")}}
+          >
+              <Text style={{color:"white"}}>
+                Go to Shopping Cart <Feather name="shopping-cart" color="white" />
+              </Text>
           </TouchableOpacity>
         </View>
-
-      </View>
-
-
-
-
-      <View style={{ padding: 30, paddingTop: 20, paddingBottom: 5 }}>
-        <Text style={{
-          color: "rgba(0,0,0,1)", fontSize: 19, fontFamily: "roboto-300"}}>{mealInfo.description}</Text>
-      </View>
-      
-      <View style={{ borderBottomColor: 'black', borderBottomWidth: 1,}}/>
-
-      <View style={{ justifyContent: "space-evenly", padding: 10, flexDirection: "row" }}>
-        <TouchableOpacity onPress={NutritionInfo}
-          style={tabSelected.nutrition ? { backgroundColor: "black" } : {}}>
-          <Text style={{ color: "rgba(106,164,27,1)", fontSize: 18, fontFamily: "roboto-900" }}>Nutrition</Text>
-        </TouchableOpacity>
         
-        <TouchableOpacity onPress={IngredientsList}
-          style={tabSelected.ingredients ? { backgroundColor: "black" } : {}}>
-          <Text style={{ color: "rgba(106,164,27,1)", fontSize: 18, fontFamily: "roboto-900"}}>Ingredients</Text>
-        </TouchableOpacity>
-       
-        <TouchableOpacity onPress={ProcedureInfo}
-          style={tabSelected.procedure ? { backgroundColor: "black" } : {}}>
-          <Text style={{ color: "rgba(106,164,27,1)", fontSize: 18, fontFamily: "roboto-900"}}>Recipe/Procedure</Text>
-        </TouchableOpacity>
       </View>
 
 
-      <ScrollView>
-        <View style={styles.infoContainer}>
-          {returnDisplayText()}
-        </View>
-      </ScrollView>
+
+
+    <View style={{padding:30, paddingTop:20, paddingBottom:5}}>
+      <Text style={{color: "rgba(0,0,0,1)",
+            fontSize: 19,
+            fontFamily: "roboto-300"}}
+            >{mealInfo.description}</Text>
+    </View>
+    <View
+      style={{
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+      }}
+    />
+    <View style={{justifyContent:"space-evenly",padding:10, flexDirection:"row"}}>
+      <TouchableOpacity
+        onPress={NutritionInfo}
+        style={tabSelected.nutrition? {backgroundColor:"black"}:{}}
+      >
+        <Text style={{
+          color: "rgba(106,164,27,1)",
+          fontSize: 18,
+          fontFamily: "roboto-900"}}>Nutrition</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={IngredientsList}
+        style={tabSelected.ingredients? {backgroundColor:"black"}: {}}
+      >
+        <Text style={{
+          color: "rgba(106,164,27,1)",
+          fontSize: 18,
+          fontFamily: "roboto-900"}}>Ingredients</Text>
+      </TouchableOpacity>
+      {/* <TouchableOpacity
+        onPress={RecipesList}
+        style={tabSelected.recipes? {backgroundColor:"black"}: {}}
+      >
+        <Text style={{
+          color: "rgba(106,164,27,1)",
+          fontSize: 18,
+          fontFamily: "roboto-900"}}>Recipes</Text>
+      </TouchableOpacity> */}
+      <TouchableOpacity
+        onPress={ProcedureInfo}
+        style={tabSelected.procedure? {backgroundColor:"black"}: {}}
+      >
+        <Text style={{
+          color: "rgba(106,164,27,1)",
+          fontSize: 18,
+          fontFamily: "roboto-900"}}>Recipe/Procedure</Text>
+      </TouchableOpacity>
+
+    </View>
+
+    
+    <ScrollView>
+      <View style={styles.infoContainer}>
+        {/* <Text>{mealInfo.name}</Text> */}
+        {returnDisplayText()}
+        
+      </View>
 
     </ScrollView>
 
+    </ScrollView>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flexDirection: "column",
-  //   flex: 1
-  // },
-  // submitButton: {
-  //   alignSelf: "flex-end",
-  //   position: 'absolute',
-  //   bottom: 30,
-  //   // left: 200, 
-  // },
-  // infoContainer: {
-  //   // alignContent:"center",
-  //   // alignSelf:"center",
-  //   // alignItems:"center",
-  //   textAlign: "center",
-  //   marginLeft: 23
-  // },
+  container: {
+    flex: 1
+  },
+  submitButton: {
+    alignSelf:"flex-end",
+    position: 'absolute',
+    bottom:30,
+    // left: 200, 
+  },
+  infoContainer:{
+    // alignContent:"center",
+    // alignSelf:"center",
+    // alignItems:"center",
+    textAlign:"center",
+    marginLeft: 23
+  },
+  group: {
+    // width: 364,
+    // height: 312,
+
+    // alignSelf: "flex-end",
+    // marginTop: -13,
+    // marginRight: 25
+  },
   image: {
     alignSelf:"center",
-    width: width,
-    height: wp("50%"),
-    marginTop: 0
+    width: wp("100%"),
+    height: hp("25%")
   },
-  // loremIpsum: {
-  //   color: "#121212",
-  //   fontSize: 27,
-  //   fontFamily: "roboto-500",
-  //   marginLeft: 1
-  // },
-  // icon: {
-  //   color: "rgba(106,164,27,1)",
-  //   fontSize: 35
-  // },
-  // loremIpsumColumn: {
-  //   width: 239,
-  //   marginBottom: 1
-  // },
-  // loremIpsumColumnFiller: {
-  //   flex: 1,
-  //   flexDirection: "row"
-  // },
+  loremIpsum: {
+    color: "#121212",
+    fontSize: 27,
+    fontFamily: "roboto-500",
+    marginLeft: 1
+  },
+  icon: {
+    color: "rgba(106,164,27,1)",
+    fontSize: 35
+  },
+  loremIpsumColumn: {
+    width: 239,
+    marginBottom: 1
+  },
+  loremIpsumColumnFiller: {
+    flex: 1,
+    flexDirection: "row"
+  },
   loremIpsum3: {
-    flexDirection: 'column',
-    paddingLeft: 10,
-    color: "#a1c559",
+    // alignItems:"flex-start",
+    // alignSelf:"flex-end",
+    // alignContent:"flex-end",
+    paddingLeft: 30,
+    color: "rgba(106,164,27,1)",
     fontSize: 30,
     fontFamily: "impact-regular"
   },
-  // materialButtonDanger: {
-  //   width: 100,
-  //   height: 36,
-  //   marginTop: 19,
-  //   marginRight: 2
-  // },
-  // loremIpsum3Column: {
-  //   width: 102,
-  //   alignItems: "flex-end",
-  //   marginTop: 3
-  // },
-  // loremIpsumColumnRow: {
-  //   height: 90, //88
-  //   flexDirection: "row",
-  //   marginTop: 3,
-  //   marginLeft: 22,
-  //   marginRight: 24
-  // },
-  // group2: {
-  //   width: 364,
-  //   height: 18,
-  //   flexDirection: "row",
-  //   marginTop: 70, //87
-  //   marginLeft: 23
-  // },
-  // button2: {
-  //   width: 72,
-  //   height: 18
-  // },
-  // nutrition: {
-  //   color: "rgba(106,164,27,1)",
-  //   fontSize: 18,
-  //   fontFamily: "roboto-900"
-  // },
-  // button52: {
-  //   width: 93,
-  //   height: 18,
-  //   marginLeft: 14
-  // },
-  // button522: {
-  //   width: 93,
-  //   height: 18
-  // },
-  // ingredients: {
-  //   color: "rgba(106,164,27,1)",
-  //   fontSize: 18,
-  //   fontFamily: "roboto-900",
-  // },
-  // button53: {
-  //   width: 65,
-  //   height: 18,
-  //   marginLeft: 18
-  //},
-  // recipes: {
-  //   color: "rgba(106,164,27,1)",
-  //   fontSize: 18,
-  //   fontFamily: "roboto-900"
-  // },
-  // button54: {
-  //   width: 89,
-  //   height: 18,
-  //   flexDirection: "row",
-  //   marginLeft: 13
-  // },
-  // procedureFiller: {
-  //   flex: 1,
-  //   flexDirection: "row"
-  // },
-  // procedure: {
-  //   color: "rgba(106,164,27,1)",
-  //   fontSize: 18,
-  //   fontFamily: "roboto-900",
-  //   width: 89
-  // },
-  // button2Row: {
-  //   height: 18, //18
-  //   flexDirection: "row",
-  //   flex: 1
-  // },
-  // loremIpsum4: {
-  //   width: 364,
-  //   height: 71, //71
-  //   color: "rgba(0,0,0,1)",
-  //   fontSize: 19,
-  //   fontFamily: "roboto-300",
-  //   marginTop: -90, //-90
-  //   marginLeft: 23
-  // }
+  materialButtonDanger: {
+    width: 100,
+    height: 36,
+    marginTop: 19,
+    marginRight: 2
+  },
+  loremIpsum3Column: {
+    width: 102,
+    alignItems: "flex-end",
+    marginTop: 3
+  },
+  loremIpsumColumnRow: {
+    height: 90, //88
+    flexDirection: "row",
+    marginTop: 3,
+    marginLeft: 22,
+    marginRight: 24
+  },
+  group2: {
+    width: 364,
+    height: 18,
+    flexDirection: "row",
+    marginTop: 70, //87
+    marginLeft: 23
+  },
+  button2: {
+    width: 72,
+    height: 18
+  },
+  nutrition: {
+    color: "rgba(106,164,27,1)",
+    fontSize: 18,
+    fontFamily: "roboto-900"
+  },
+  button52: {
+    width: 93,
+    height: 18,
+    marginLeft: 14
+  },
+  button522: {
+    width: 93,
+    height: 18
+  },
+  ingredients: {
+    color: "rgba(106,164,27,1)",
+    fontSize: 18,
+    fontFamily: "roboto-900",
+  },
+  button53: {
+    width: 65,
+    height: 18,
+    marginLeft: 18
+  },
+  recipes: {
+    color: "rgba(106,164,27,1)",
+    fontSize: 18,
+    fontFamily: "roboto-900"
+  },
+  button54: {
+    width: 89,
+    height: 18,
+    flexDirection: "row",
+    marginLeft: 13
+  },
+  procedureFiller: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  procedure: {
+    color: "rgba(106,164,27,1)",
+    fontSize: 18,
+    fontFamily: "roboto-900",
+    width: 89
+  },
+  button2Row: {
+    height: 18, //18
+    flexDirection: "row",
+    flex: 1
+  },
+  loremIpsum4: {
+    width: 364,
+    height: 71, //71
+    color: "rgba(0,0,0,1)",
+    fontSize: 19,
+    fontFamily: "roboto-300",
+    marginTop: -90, //-90
+    marginLeft: 23
+  }
 });
 
 export default DetailMeal;
