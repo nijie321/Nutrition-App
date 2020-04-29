681d9caa7e1aed9ce10af005c0c2539280914a5f
Jacob <jpoyner1@bitbucket.com>
2020-04-29 18:28:18 -0500

    Updated star rating system to send star document to database

--- src/Components/DetailedMeal/DetailedMeal/src/screens/DetailMeal.js
+++ src/Components/DetailedMeal/DetailedMeal/src/screens/DetailMeal.js
@@ -8,11 +8,17 @@ import {
   TouchableOpacity,
   ScrollView,
   TextInput,
-  Alert
+  KeyboardAvoidingView,
+  Dimensions
 } from "react-native";
+// import Icon from "react-native-vector-icons/EvilIcons";
+// import MaterialButtonDanger from "../components/MaterialButtonDanger";
 import {Picker,Icon} from 'native-base';
+
 import { useRoute, useNavigation } from "@react-navigation/native";
+
 import firebase from '../../../../../../FireBase';
+
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Feather from 'react-native-vector-icons/Feather';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -23,12 +29,16 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 //import {StarRating} from 'react-native-rating-star';
 import { AirbnbRating } from 'react-native-ratings';
 
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-function DetailMeal() {
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+const db = firebase.firestore();
+
+// const width = Dimensions.get("window").width;
+
+
+function DetailMeal(props) {
+
   
-  const db = firebase.firestore();
-  const user = firebase.auth().currentUser;
   const route = useRoute();
   const [display, setDisplay] = useState({toDisplay: ""});
   const [pickUpLocation, setPickUpLocation] = useState("");
@@ -36,6 +46,11 @@ function DetailMeal() {
   const [imgSrc, setImgSrc] = useState();
   const [mealInfo, setMealInfo] = useState({});
   const [qty, setQty] = useState(0);
+
+  
+  //const [value, setValue] = useSate(0);
+
+
   const [tabSelected, setTabSelected] = useState({
     nutrition: false,
     ingredients: false,
@@ -71,25 +86,20 @@ function DetailMeal() {
 
 
 
-  function ratingCompleted( rating ) {
-    console.log( `Rating is: ${rating}` );
-  }
-
-
-
   function returnIngredientString(info, procedure = false) {
-    let splitString = ""
-    if(procedure) splitString = ". ";
-    else splitString = ", ";
-    return info.split(splitString).reduce((acc, cur) => {acc += cur + "\n"; return acc}, "");
+    if (procedure) {
+      return info.split(". ").reduce((acc, cur) => { acc += cur + "\n"; return acc }, "");
+    } else {
+      return info.split(", ").reduce((acc, cur) => { acc += cur + "\n"; return acc }, "");
+    }
   }
 
   function returnDisplayText() {
     switch (display.toDisplay) {
-      case "ingredients": return (<Text>{returnIngredientString(mealInfo.ingredients)}</Text>);
-      case "recipe": return (<Text>recipe....</Text>);
-      case "nutrition": return (<Text>{returnIngredientString(nutritionText)}</Text>);
-      case "procedures": return (<Text>{returnIngredientString(mealInfo.procedure, true)}</Text>);
+      case "ingredients": return (<Text>{returnIngredientString(mealInfo.ingredients)}</Text>); break;
+      case "recipe": return (<Text>recipe....</Text>); break;
+      case "nutrition": return (<Text>{returnIngredientString(nutritionText)}</Text>); break;
+      case "procedures": return (<Text>{returnIngredientString(mealInfo.procedure, true)}</Text>); break;
     }
   }
 
@@ -127,8 +137,34 @@ function DetailMeal() {
     })
   }
   
+
+
+  function ratingCompleted( rating ) {
+    const meal_id = "meal 8";
+    const user_id = firebase.auth().currentUser.uid;
+    const value = rating;
+
+    console.log( `Rating is: ${rating}` );
+    console.log( `user_id is: ${user_id}` );
+    console.log( `meal_id is: ${meal_id}` );
+
+    var docData = {
+      meal_id: meal_id,
+      user_id: user_id,
+      value: value,
+    }
+    console.log("docData:", docData);
+    db.collection("stars").doc(user_id.concat(meal_id)).update(docData)
+    .then(function(){
+      console.log("update to shopping cart successfully.");
+    })
+  }
+
+
+
   function addToCart(){
-    if(qty > 0 && pickUpLocation !== ""){
+    if(qty > 0){
+      const user = firebase.auth().currentUser;
       var docData = {
         [route.params.meal_info]: {
           qty,
@@ -138,22 +174,52 @@ function DetailMeal() {
       console.log("docData:", docData);
       db.collection("shopping_cart").doc(user.uid).update(docData)
       .then(function(){
-        Alert.alert("Added to shopping cart successfully.");
+        console.log("update to shopping cart successfully.");
       })
-      .catch(() => {
+      .catch(function(error){
         db.collection("shopping_cart").doc(user.uid).set(docData)
         .then(function(){
           console.log("add to shopping cart successfully.");
         })
       })  
     }else{
-      Alert.alert("Please make sure the quantity is above 0 and/or the pick-up locaiton is entered.");
+      Alert.alert("The quantity must be greater than 0");
     }
   }
 
-  function addToFavorite2() {
+
+  function addToCart() {
+    const user = firebase.auth().currentUser;
     var docData = {
-      [route.params.meal_info]: 1
+      [route.params.meal_info]: qty
+    }
+    console.log("docData:", docData);
+    db.collection("shopping_cart").doc(user.uid).update(docData)
+      .then(function () {
+        console.log("update to shopping cart successfully.");
+      })
+      .catch(function (error) {
+        db.collection("shopping_cart").doc(user.uid).set(docData)
+          .then(function () {
+            console.log("add to shopping cart successfully.");
+          })
+        // console.log(error);
+        // console.log("failed adding meal to cart.");
+      })
+    // console.log(qty);
+    // console.log(route.params.meal_info)
+  }
+
+  function directToCart() {
+
+  }
+
+  //add to favorite
+
+  function addToFavorite2() {
+    const user = firebase.auth().currentUser;
+    var docData = {
+      [route.params.meal_info]: qty
     }
     console.log("docData:", docData);
     db.collection("favorite").doc(user.uid).update(docData)
@@ -165,17 +231,35 @@ function DetailMeal() {
           .then(function () {
             console.log("add to favorite successfully.");
           })
+        
       })
+   
   }
 
   //remove to favorite
 
   function removeToFavorite2() {
-    db.collection("favorite").doc(user.uid).update({
-      [ route.params.meal_info ]: firebase.firestore.FieldValue.delete()
-    })
-    .then(() => {console.log("delete successfully")})
-    .catch((err) => {console.log(err)});
+    const user = firebase.auth().currentUser;
+    var docData = {
+      [route.params.meal_info]: qty
+    }
+    console.log("docData:", docData);
+    db.collection("favorite").doc(user.uid).update(docData)
+      .then(function () {
+        console.log("remove to favorite successfully.");
+      })
+      .catch(function (error) {
+        db.collection("favorite").doc(user.uid).set(docData)
+          .then(function () {
+            console.log("remove to favorite successfully.");
+          })
+        
+      })
+   
+  }
+
+  function directToFavorite2() {
+
   }
 
   useEffect(() => {
@@ -184,8 +268,10 @@ function DetailMeal() {
     setBackButton();
   }, []);
 
+  // getMealInfo();
 
   return (
+          
     <ScrollView style={styles.container}>
       <View style={styles.group}>
         <Image
@@ -238,6 +324,19 @@ function DetailMeal() {
                       onChangeText={text => setQty(text)}
                       />
           
+          {/* <Text style={{fontWeight:"bold", fontSize:20, alignSelf:"center", paddingLeft:wp("5%")}}>
+            Pick-up Location:
+          </Text>
+          <TextInput style={{borderWidth:1 ,marginTop:5, width:wp("30%"), height:hp("4%"), marginLeft:wp("1%"), alignSelf:"center"}}/> */}
+          
+          {/* <TouchableOpacity style={{backgroundColor:"rgba(106,164,27,1)", width:wp("1%"), alignItems:"center", marginLeft:wp("5%") , padding:10, borderRadius:10}}
+                onPress={() => {navigation.navigate("Shopping Cart")}}
+          >
+              <Text style={{color:"white"}}>
+                Go to Shopping Cart
+              </Text>
+          </TouchableOpacity> */}
+
           <TouchableOpacity style={{backgroundColor:"rgba(106,164,27,1)", width:wp("20%"), alignItems:"center", padding:10, borderRadius:10, position:"absolute", alignSelf:"center", right:wp("1%")}}
                 onPress={addToCart}
           >
@@ -248,6 +347,9 @@ function DetailMeal() {
         </View>
 
         <View style={{flexDirection:"row", marginBottom:hp("2%")}}>
+          {/* <Text style={{fontWeight:"bold", fontSize:20, paddingLeft:wp("5%")}}>
+            Pick-up Location:
+          </Text> */}
 
             <View style={{width: wp("30%") ,marginLeft:wp("5%")}}>
                     <Picker
@@ -267,6 +369,7 @@ function DetailMeal() {
                     <Picker.Item label="address 5" value="address 5" />
                 </Picker>
             </View>
+          {/* <TextInput style={{borderWidth:1 ,marginTop:5, width:wp("30%"), height:hp("3%"), marginLeft:wp("1%")}}/>  */}
 
            <TouchableOpacity style={{backgroundColor:"rgba(106,164,27,1)", width:wp("20%"), alignItems:"center", marginLeft:wp("5%") , padding:10, borderRadius:10, position:"absolute",right:wp("1%")}}
                 onPress={() => {navigation.navigate("Shopping Cart")}}
@@ -278,6 +381,9 @@ function DetailMeal() {
         </View>
         
       </View>
+
+
+
 
     <View style={{padding:30, paddingTop:20, paddingBottom:5}}>
       <Text style={{color: "rgba(0,0,0,1)",
@@ -310,6 +416,15 @@ function DetailMeal() {
           fontSize: 18,
           fontFamily: "roboto-900"}}>Ingredients</Text>
       </TouchableOpacity>
+      {/* <TouchableOpacity
+        onPress={RecipesList}
+        style={tabSelected.recipes? {backgroundColor:"black"}: {}}
+      >
+        <Text style={{
+          color: "rgba(106,164,27,1)",
+          fontSize: 18,
+          fontFamily: "roboto-900"}}>Recipes</Text>
+      </TouchableOpacity> */}
       <TouchableOpacity
         onPress={ProcedureInfo}
         style={tabSelected.procedure? {backgroundColor:"black"}: {}}
@@ -348,12 +463,19 @@ const styles = StyleSheet.create({
     // left: 200, 
   },
   infoContainer:{
+    // alignContent:"center",
+    // alignSelf:"center",
+    // alignItems:"center",
     textAlign:"center",
     marginLeft: 23
   },
   group: {
     // width: 364,
     // height: 312,
+
+    // alignSelf: "flex-end",
+    // marginTop: -13,
+    // marginRight: 25
   },
   image: {
     alignSelf:"center",
